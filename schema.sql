458d1f4a2a17275d1b4f809dd4f04d3fbfd1fe70
ANKIT PAWAR <56432722+ankit8453@users.noreply.github.com>
2024-03-11 15:55:51 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,43 @@
+CREATE DATABASE PRODUCT;
 
+USE PRODUCT;
+
+CREATE TABLE product_category (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP,
+    modified_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+CREATE TABLE product_inventory (
+    id INT PRIMARY KEY,
+    quantity INT,
+    created_at TIMESTAMP,
+    modified_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+CREATE TABLE discount (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    discount_percent DECIMAL(5, 2),
+    active BOOLEAN,
+    created_at TIMESTAMP,
+    modified_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE product (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    descRIPTION TEXT,
+    SKU VARCHAR(255),
+    category_id INT REFERENCES product_category(id),
+    inventory_id INT REFERENCES product_inventory(id),
+    price DECIMAL(10, 2),
+    discount_id INT REFERENCES discount(id),
+    created_at TIMESTAMP,
+    modified_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
